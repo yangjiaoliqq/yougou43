447f6b2595dab368cc0704e6e578e52052e99ede
杨 <1512597921@qq.com>
2020-02-09 12:03:58 +0800

    右侧静态页面

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,14 +2,29 @@
   <div class="categoryBox">
     <!-- 搜索框 -->
     <SearchLink />
-    <!-- 商品分类 -->
+    <!-- 内容区域 -->
     <div class="content">
+      <!-- 左侧一级菜单 -->
       <ul class="left">
         <li :class="{active:activeIndex===index}" @click="activeIndex=index" v-for="(item,index) in 20" :key="index">大家电</li>
       </ul>
-      <ul class="right">
-        <li>华为</li>
-      </ul>
+      <!-- 右侧内容 -->
+      <div class="right">
+        <img src="/static/images/titleImage.png" alt="">
+        <!-- 二级菜单 -->
+        <ul>
+          <li class="cate2" v-for="(cate2,index2) in 4" :key="index2">
+            <p class="title">/<span>电视</span>/</p>
+              <!-- 三级菜单 -->
+              <ul>
+                <li class="cate3" v-for="(cate3,index3) in 9" :key="index3">
+                  <img src="https://api.zbztb.cn/full/2fb113b32f7a2b161f5ee4096c319afedc3fd5a1.jpg" alt="">
+                  <p>曲面电视</p>
+                </li>
+              </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +45,7 @@ export default {
 </script>
 
 <style lang="less">
+// 内容区域
 .content {
   display: flex;
   position: absolute;
@@ -38,6 +54,7 @@ export default {
   // left: 0;
   // right: 0;
   width: 100%;
+  // 左侧菜单
   .left {
     width: 198rpx;
     overflow: scroll;
@@ -65,8 +82,42 @@ export default {
       }
     }
   }
+  // 右侧内容
   .right {
     flex: 1;
+    padding: 20rpx 16rpx 0;
+    overflow: scroll;
+    img {
+      width: 520rpx;
+      height: 180rpx;
+    }
+      .cate2 {
+       .title {
+        color: #e0e0e0;
+        height: 110rpx;
+        line-height: 110rpx;
+        text-align: center;
+        span {
+          color: #333;
+          margin: 0 30rpx;
+        }
+      }
+     ul {
+      display: flex;
+      flex-wrap: wrap;
+     }
+    }
+    // 三级菜单
+   .cate3 {
+      width: 33.33%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      img {
+      width: 120rpx;
+      height: 120rpx;
+     }
+    }
   }
 }
 
